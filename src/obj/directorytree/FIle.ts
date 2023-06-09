8d879c114f67c354de4c7c34eb5fbfedfb31dff5
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-06-09 14:14:58 -0400

    added methods to File.tsx

--- src/obj/directorytree/FIle.ts
+++ src/obj/directorytree/FIle.ts
@@ -1,9 +1,25 @@
 export class File {
     private _name: string;
-    _onRun: () => void;
+    private _onRun: () => void;
 
     constructor(name: string, onRun: () => void) {
         this._name = name;
         this._onRun = onRun;
+    }
+
+    /**
+     * @summary calls the private onRun Function
+     * @returns {void} onRun Function
+     */
+    public call() {
+        return this._onRun();
+    }
+
+    /**
+     * @summary Returns the name of the file
+     * @returns {string} Returns the name of the file
+     */
+    public getName() : string { 
+        return this._name; 
     }
 }
