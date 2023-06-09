d123717cc4ff8dc50665af9f2705070e4d64f4c3
Kevin <identityofsine@gmail.com>
2023-06-09 19:35:51 -0400

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://identityofsine.github.io/web-dos-page',
+  base:'https://identityofsine.github.io/web-dos-base/',
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg']
 })
