0dbda2225ba75a21155c91548a9491e2b0c3f93d
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-06-14 13:38:08 -0400

    disabled text engine for input fields

--- src/assets/elements/dos-text/dos-text.tsx
+++ src/assets/elements/dos-text/dos-text.tsx
@@ -3,17 +3,21 @@ import "./dos-text.scss";
 import { TextEngine } from '../../../textengine/engine';
 
 /**
- * @summary This is a function that is responsible for displaying dos text onto the screen
- * @param {string} text 
- * @returns DosText Element
- */
-const DosText = ({text = "", color = 'white' || 'yellow' || 'red'}) => {
-    return (
-        <div className='dos-text'>
-            {/* <span style={{color:color === 'white' ? '#ffffff' : color === 'yellow' ? '#ffff00' : color === 'red' ? '#ff0000' : '#ffffff'}}>{text}</span> */}
-            {new TextEngine.MarkedText(text).returnElement()}
-        </div>
-    )
-}
-
-export default DosText
+* @summary This is a function that is responsible for displaying dos text onto the screen
+* @param {string} text 
+* @returns DosText Element
+*/
+const DosText = ({text = "", color = 'white' || 'yellow' || 'red', disableTextEngine = false}) => {
+	return (
+		<div className='dos-text'>
+			{disableTextEngine ? 
+				<span style={{color:color === 'white' ? '#ffffff' : color === 'yellow' ? '#ffff00' : color === 'red' ? '#ff0000' : '#ffffff'}}>
+					{text}
+				</span> 
+			: 
+				new TextEngine.MarkedText(text).returnElement()}
+		</div>
+			)
+		}
+		
+		export default DosText
