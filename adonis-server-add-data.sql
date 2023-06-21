ab6ab76cecf8d64d5d16307f9a062dfa73a9d6ce
F/X/ <kevinanony.main@gmail.com>
2023-06-21 18:31:34 -0400

    added data into sql

--- adonis-server-add-data.sql
+++ adonis-server-add-data.sql
@@ -3,3 +3,9 @@ INSERT INTO file_system () VALUES ();
 
 -- @block
 INSERT INTO directory (name) VALUES ('featured');
+
+-- @block
+INSERT INTO files (name, parent_id, blog_id) VALUES ('example', 1, 1);
+
+-- @block
+INSERT INTO blog (title, image, content, created_at, updated_at) VALUES ('Welcome to Adonis', 'https://i.imgur.com/azibwYe.jpeg', 'Welcome to Adonis, a blogging platform built on top of WDB (WEB-DOS-BASE). This is the default blog post, which you can edit or delete from the admin panel.', NOW(), NOW());
