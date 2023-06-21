580ab7e86f51486f5f95ca0cd46efbec1b72020c
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-06-21 01:53:47 -0400

    added next function for connect SQLConnection

--- db/sqlconnection.ts
+++ db/sqlconnection.ts
@@ -9,7 +9,7 @@ class SQLConnection {
 	 * @summary This function connects to the database and uses the 'callback_on_connection' variable to run code on connection, that may need the database.
 	 * @param callback_on_connection 
 	 */
-	connect(callback_on_connection: (connection_object : mysql.Connection) => void, on_error?: (err: any) => void): void{
+	connect(callback_on_connection: (connection_object : mysql.Connection, sql_next : () => void) => void, on_error?: (err: any) => void): void{
 		if(!this.currentConnection) {
 			this.currentConnection = this.createConnection();
 			if(isDebug)
@@ -22,15 +22,19 @@ class SQLConnection {
 				on_error(error);
 				return;
 			}
-			callback_on_connection(this.currentConnection);
+			callback_on_connection(this.currentConnection, () => this.closeConnection(this.currentConnection, on_error));
 		});
 
 	}
 
-	private closeConnection() {
-		
+	private closeConnection(connection_object : mysql.Connection, on_error? : (err : mysql.MysqlError) => void) {
+		connection_object.end(on_error);
 	}
 
+	/**
+	 * @summary Create Connection using env variables
+	 * @returns {mysql.Connection} Connection Object
+	 */
 	private createConnection(): mysql.Connection {
 		const mysql_connection = mysql.createConnection({
 			host: 'localhost',
