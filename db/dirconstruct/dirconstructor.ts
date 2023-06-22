bf0ff617e7ee5bf41f63ed9948238c432cec3693
Kevin <identityofsine@gmail.com>
2023-06-22 14:40:54 -0400

    added some type

--- db/dirconstruct/dirconstructor.ts
+++ db/dirconstruct/dirconstructor.ts
@@ -27,15 +27,18 @@ class DirectoryConstructor {
 	
 	current_sql_connection : mysql.Connection;
 	on_error_callback : (err: DatabaseError) => void;
+	all_directories : Directory[];
+	all_files : File[];
 
 	constructor(sql_connection : mysql.Connection, on_error_callback : (err: DatabaseError) => void) {
 		this.current_sql_connection = sql_connection;
 		this.on_error_callback = on_error_callback;
+		this.all_directories = [];
 	}
 
-	async grabAllDirectories(setter_function : (passed_in_value : any[]) => void) {
+	async grabAllDirectories(setter_function : (passed_in_value : Directory[]) => void) {
 		let query_result : [];
-		this.current_sql_connection.query("SELECT * FROM directory", (err, result) => {
+		this.current_sql_connection.query("SELECT * FROM directory", (err, result: Directory[]) => {
 			if(err) {
 				if(isDebug) {
 					console.log("❌ [MYSQL] Error occured while grabbing all directories: " + err.message);
@@ -54,7 +57,7 @@ class DirectoryConstructor {
 		}
 		this.grabAllDirectories((result) => {
 			for(let item in result) {
-				
+				this.all_directories.push(item);
 			}
 		});
 	}
