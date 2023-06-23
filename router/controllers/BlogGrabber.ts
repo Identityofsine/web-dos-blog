b611713859e042c4d20eacf572237fe8cfef521c
F/X/ <identityofsine@gmail.com>
2023-06-23 00:26:01 -0400

    added 404 detection

--- router/controllers/BlogGrabber.ts
+++ router/controllers/BlogGrabber.ts
@@ -3,12 +3,12 @@ import GrabBlog from "../../db/grabblog/grabblog";
 import SQLConnection from "../../db/sqlconnection";
 import { isDebug } from "../..";
 
-async function GetBlog(req : Request, res : Response) {
+export async function GetBlog(req : Request, res : Response) {
 
 	const methodSignature = 'getblog';
 
-	let blog_id = parseInt(req.query.blog_id as string);
-	if(!blog_id) return res.status(400).json({message: "No blog id provided."});
+	const blog_id = parseInt(req.query.blog_id as string);
+	if(blog_id == undefined) return res.status(400).json({message: "No blog id provided."});
 
 
 	const sql_connection = SQLConnection.getInstance();
@@ -16,6 +16,9 @@ async function GetBlog(req : Request, res : Response) {
 		GrabBlog(sql_connection_object, blog_id, (blog_json) => {
 			if(isDebug)
 				console.log('✅ [MYSQL:%s] Grabbed blog with id %d', methodSignature, blog_id);
+			if(blog_json == undefined) {
+				res.status(404).json({message: "Blog not found."});
+			}
 			res.status(200).json(blog_json);
 			on_next();
 		});
--- router/routes/dir.routes.ts
+++ router/routes/dir.routes.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import { GetFileStructure } from '../controllers/DirectoryController';
+import { GetBlog } from '../controllers/BlogGrabber';
 
 const dir_router = Router();
 
 dir_router.get("/getfilestructure", GetFileStructure);
+dir_router.get("/getblog", GetBlog);
 
 
 export default dir_router;
